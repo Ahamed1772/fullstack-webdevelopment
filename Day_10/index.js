257429be474db76c9432742ae2023db910e7d83f
Ahamed IbrahimKhan <ahamedibrahim1772@gmail.com>
2025-06-08 22:49:59 +0530

    Day9 and Day10 completed successfully

---
+++ Day_10/index.js
@@ -0,0 +1,72 @@
+
+// console.log("hello world");
+
+//leapyear or not
+
+
+// function isLeepYear(year){
+//     if (year % 4 === 0){
+//         if (year % 100 === 0){
+//             if (year % 400 === 0){
+//                 console.log("It's a leap Year");
+//             }
+//         }else{
+//             console.log("It's a leap Year");
+//         }
+//     }else{
+//         console.log("NOt a leap Year");
+//     }
+// }
+
+
+// isLeepYear(2008);
+
+//is he in the list
+
+// var guestlist = ["ahamed","ibrahim","mari","bharthi","bala","prasanth"];
+// var name = prompt("what's your name? ");
+// if (guestlist.includes(name)){
+//     alert("you are welcome");
+// }else{
+//     alert("you are not welcome");
+// }
+
+
+// who is going to pay 
+// var names = ["Angela","Ben","Jenny","Michael"];
+
+// function whoispaying(){
+//     var random = Math.round(Math.random() * (names.length-1));
+//     var valid = names[random];
+//     console.log(valid + " is going to pay for dinner");
+// }
+
+// whoispaying();
+
+
+// loop
+
+// var count = 1;
+// var arraylist = [];
+// while (count <= 100){
+//     arraylist.push(count);
+//     count++;
+// }
+
+// console.log(arraylist);
+
+// var count = 100;
+
+// while (count >= 0){
+//     if (count == 0){
+//         console.log("no more bootles of beer on the wall")
+//     }else{
+//         console.log(count + " bootles of beer on the wall");
+//     }
+//     count--;
+// }
+
+//for loop
+// for (var i = 0; i <= 100;i++){
+//     console.log(i);
+// }
